4a1949d6c0bbb52a403e864dbad997f633ef9efe
dotunalukosprinble <dotuna@sprinble.co>
2019-03-23 10:22:54 +0100

    Added Layout to App Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
+import Layout from "./containers/Layout";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>App Component</h1>
-      </div>
-    );
-  }
+export default function App() {
+  return <Layout />;
 }
-
-export default App;
--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import Home from "../components/Home";
 import Login from "../components/auth/Login";
 import Logout from "../components/auth/Logout";
 
+// TODO: Add Suspense here
 export default class Layout extends Component {
   render() {
     return (
       <Switch>
-        {/* <Route path="/" component="" />
-        <Route path="/login" component="" />
-        <Route path="/logout" component="" /> */}
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/logout" component={Logout} />
       </Switch>
     );
   }
