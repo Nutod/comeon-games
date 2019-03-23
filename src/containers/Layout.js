6b7559052c1f3cf9f679a75671fba3066b05fd3c
dotunalukosprinble <dotuna@sprinble.co>
2019-03-23 10:18:53 +0100

    Added auth files

---
+++ src/containers/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
+export default class Layout extends Component {
+  render() {
+    return (
+      <Switch>
+        {/* <Route path="/" component="" />
+        <Route path="/login" component="" />
+        <Route path="/logout" component="" /> */}
+      </Switch>
+    );
+  }
+}
