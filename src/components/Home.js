e7a0f1780954b502f95e1a9bc4920adde4ee7400
dotunalukosprinble <dotuna@sprinble.co>
2019-03-23 10:27:57 +0100

    Added new Header Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Logo from "../components/assets/images/logo.svg";
 
 export default function Home() {
   return (
     <div class="ui one column center aligned page grid">
       <div class="column twelve wide">
-        <img src="images/logo.svg" alt="logo" />
+        <img src={Logo} alt="logo" />
       </div>
     </div>
   );
