f222bfdade088812fc9548d35a4f4a5bdfb76db7
dotunalukosprinble <dotuna@sprinble.co>
2019-03-23 10:57:35 +0100

    Added Search view

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div class="four wide column">
+      <div class="search ui small icon input ">
+        <input type="text" placeholder="Search Game" />
+        <i class="search icon" />
+      </div>
+    </div>
+  );
+}
