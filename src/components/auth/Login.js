fa001c99168812edbd15d937b2d3910269f14851
dotunalukosprinble <dotuna@sprinble.co>
2019-03-23 10:35:38 +0100

    Added Login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,8 +2,31 @@ import React from "react";
 
 export default function Login() {
   return (
-    <div>
-      <h2>Login</h2>
+    <div class="login" style={{ display: "block", marginTop: "50%" }}>
+      <div class="ui grid centered">
+        <form>
+          <div class="fields">
+            <div class="required field">
+              <div class="ui icon input">
+                <input type="text" name="username" placeholder="Username" />
+                <i class="user icon" />
+              </div>
+            </div>
+            <div class="required field">
+              <div class="ui icon input">
+                <input type="password" name="password" placeholder="Password" />
+                <i class="lock icon" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="ui icon input">
+                <input type="submit" value="Login" />
+                <i class="right chevron icon" />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
