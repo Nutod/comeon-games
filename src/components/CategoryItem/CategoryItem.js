eb5911eb73f30ae47ae2c4e008d9bc12aaa316cd
dotunalukosprinble <dotuna@sprinble.co>
2019-03-23 12:29:09 +0100

    Added proptype validation to CategoryItem

--- src/components/CategoryItem/CategoryItem.js
+++ src/components/CategoryItem/CategoryItem.js
@@ -1,13 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function CategoryItem({ header }) {
   return (
     <div class="category item">
       <div class="content">
         <div class="header">
-          <h4>{header}</h4>
+          <h4>{header.toUpperCase()}</h4>
         </div>
       </div>
     </div>
   );
 }
+
+CategoryItem.propTypes = {
+  header: PropTypes.string.isRequired
+};
