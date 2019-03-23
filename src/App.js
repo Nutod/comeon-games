ba86b952873fd5d92cf61f6cdda88d86b70e19bd
dotunalukosprinble <dotuna@sprinble.co>
2019-03-23 10:13:07 +0100

    Added React Router support

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+      <div>
+        <h1>App Component</h1>
       </div>
     );
   }
