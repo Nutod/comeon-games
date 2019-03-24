8b51f2e06bea3343fdaf257fbafd3d4a2cdfdae3
Nutod <alukodotun@gmail.com>
2019-03-25 00:50:56 +0100

    Added new Todo to Login test suite

--- src/components/auth/Login.test.js
+++ src/components/auth/Login.test.js
@@ -1,3 +1,13 @@
-describe("Login", () => {
-  expect(true).toBe(true)
-})
+import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import Login from "./Login";
+
+// TODO: Fix smoke test
+describe("<Login /> Snapshot", () => {
+  const wrapper = shallow(<Login />);
+
+  it("Should match snapshot", () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
